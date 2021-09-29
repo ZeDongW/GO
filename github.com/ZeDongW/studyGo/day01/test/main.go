04926f79ec5b962f61e19eeec39e77ae80727555
ZeDongW <878201217@qq.com>
2021-09-30 00:05:24 +0800

    练习

--- github.com/ZeDongW/studyGo/day01/test/main.go
+++ github.com/ZeDongW/studyGo/day01/test/main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	// "strings"
+	"unicode"
 )
 
 func main() {
 
 	s1 := "hello沙河小王子hah嘿"
 
-	s2 := "abcdefghijklmnopqrstuvwxyz"
+	// s2 := "abcdefghijklmnopqrstuvwxyz"
 
 	num := 0
-	for _, c := range s1 {
+	/* for _, c := range s1 {
 		if !strings.ContainsRune(s2, c) {
+			num++
+		}
+	} */
+
+	for _, c := range s1 {
+		if unicode.Is(unicode.Han, c) {
 			num++
 		}
 	}
